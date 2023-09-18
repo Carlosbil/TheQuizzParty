f05dfd03df42bc11b772eea2bfe78d15a7ac6ff6
Carlosbil <carlosbilbao2@gmail.com>
2023-09-18 21:26:20 +0200

    Dual Begining
    Dual Begining

--- questionary/src/components/gameMenu/gameMenu.js
+++ questionary/src/components/gameMenu/gameMenu.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './gameMenu.css'
-const FunButtons = () => {
+
+function FunButtons() {
+  const handleButtonClick_menu = (tipo) => {
+    console.log(tipo) // Oculta el botón al hacer clic
+  };
   return (
     <div className="fun-buttons-container">
-      <button className="fun-button">Botón 1</button>
-      <button className="fun-button">Botón 2</button>
-      <button className="fun-button">Botón 3</button>
+      <button className="fun-button"  
+        onClick={() => handleButtonClick_menu('history')} >Preguntas de historia</button>
+      <button className="fun-button"
+        onClick={() => handleButtonClick_menu('Geography')} >Preguntas de Geografía</button>
+      <button className="fun-button"
+        onClick={() => handleButtonClick_menu('random')} >Preguntas Aleatorias</button>
     </div>
   );
 };
