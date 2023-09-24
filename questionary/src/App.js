8d5355532490fa4d2b138b4ebba9788d9f78d43a
Carlosbil <carlosbilbao2@gmail.com>
2023-09-24 21:31:26 +0200

    #6 added toastMessage for errors
    #6 added toastMessage for errors and delted some unused componets

--- questionary/src/App.js
+++ questionary/src/App.js
@@ -1,8 +1,8 @@
 // src/App.js
 import React from 'react';
 import './App.css';
-import ImageApp from './components/images/image.js';
 import Mainpage from './components/mainPage/mainPage';
+
 function App() {
   return (
     <div className="App">
