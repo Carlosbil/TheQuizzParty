961c6ea8d7192594b818494609c7a222f36b5463
Carlosbil <carlosbilbao2@gmail.com>
2023-09-25 23:10:52 +0200

    #11 home button added
    #11 home button added

--- questionary/src/components/mainPage/mainPage.js
+++ questionary/src/components/mainPage/mainPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import './main.css'
+import './main.css';
 import FunButtons from '../gameMenu/gameMenu';
+import Logo from '../homebotton/homebotton';
 
 function MainPage() {
   const [showSecondComponent, setShowSecondComponent] = useState(false);
@@ -8,12 +9,17 @@ function MainPage() {
 
   const handleButtonClick_menu = () => {
     setShowSecondComponent(true);
-    setShowButton(false); // Oculta el botón al hacer clic
+    setShowButton(false);
   };
-  
+
+  const handleLogoClick = () => {
+    setShowSecondComponent(false);
+    setShowButton(true);
+  };
 
   return (
     <div className='page'>
+      <Logo onClick={handleLogoClick} /> {/* Agregar el componente Logo aquí */}
       {showButton && <button className='fun' onClick={handleButtonClick_menu}>The quizz</button>}
       {showSecondComponent && <FunButtons />}
     </div>
