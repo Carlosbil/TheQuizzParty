f4aa9f5e9cd2120a00c5f3a7365729f94a725429
Carlosbil <carlosbilbao2@gmail.com>
2023-09-28 22:14:56 +0200

    #28 answer checked and update css
    #28 answer checked and update css

--- questionary/src/components/gameMenu/gameMenu.js
+++ questionary/src/components/gameMenu/gameMenu.js
@@ -1,33 +1,13 @@
 import React, { useState } from 'react';
 import './gameMenu.css';
-import axios from 'axios';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import QuestionDisplayer from '../questionDisplayer/questionDisplayer';
 
 function FunButtons() {
-  const [question, setQuestion] = useState('');
-  const [options, setOptions] = useState([]);
-  const [answer, setAnswer] = useState('');
   const [showQuestionDisplayer, setShowQuestionDisplayer] = useState(false);
 
   const handleButtonClick_menu = (category) => {
-    axios
-      .get('/api/questions', { params: { data: category } })
-      .then((response) => {
         setShowQuestionDisplayer(true);
-        setQuestion(decodeURIComponent(response.data.question));
-        setAnswer(decodeURIComponent(response.data.answer));
-        setOptions(decodeURIComponent(response.data.options).split(","));
-      })
-      .catch((error) => {
-        console.error('Error al realizar la solicitud:', error);
-        if (error.response === undefined || error.response.data.error === undefined) {
-          toast.error('Error al realizar la solicitud:' + error.message);
-        } else {
-          toast.error('Error al realizar la solicitud:' + error.response.data.error);
-        }
-      });
+        localStorage.setItem('category', category);
   };
 
   return (
@@ -45,8 +25,7 @@ function FunButtons() {
             </button>
         </div>
       )}
-      {showQuestionDisplayer && <QuestionDisplayer question={question} options={options} />}
-      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
+      {showQuestionDisplayer && <QuestionDisplayer />}
     </div>
   );
 }
