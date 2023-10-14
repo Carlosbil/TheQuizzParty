30e0b7125bf366891e11807e7aefe4c9707fd7f5
Carlosbil <carlosbilbao2@gmail.com>
2023-10-14 13:59:17 +0200

    #39 they are able to modify
    #39 new endpoint for save the user update information and a formdata in profile.js to introduce the new informatio

--- questionary/src/components/profile/profile.js
+++ questionary/src/components/profile/profile.js
@@ -5,19 +5,72 @@ import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Logo from '../homebotton/homebotton';
 import { useNavigate } from 'react-router-dom';
-import { PROFILE_URL } from '../../enpoints';
+import { PROFILE_URL, UPDATE_PROFILE_URL } from '../../enpoints';
 import { getCookieValue } from '../../authSlide';
+
 function Profile() {
     const [username, setUsername] = useState("");
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [showProfile, setShowProfile] = useState(false)
+    const [showProfile, setShowProfile] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [formData, setFormData] = useState({
+        token: '',
+        name: '',
+        username: '',
+        email: '',
+        password: '',
+    });
+    
+    //handle the changes produced in the form
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+    
+    //save user information
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        let data = {
+            "token": getCookieValue("auth_token"),
+            "name": formData.name !== ''? formData.name : name,
+            "email": formData.email !== ''? formData.email : email,
+            "username": formData.username !== ''? formData.username : username,
+            "password": formData.password !== ''? formData.password : password
+        }
+        axios
+            .put(UPDATE_PROFILE_URL, data)
+            .then((response) => {
+                setUsername(decodeURIComponent(response.data.username));
+                setEmail(decodeURIComponent(response.data.email));
+                setName(decodeURIComponent(response.data.name));
+                setPassword(decodeURIComponent(response.data.password));
+                setShowProfile(true);
+                setIsEditing(false)
+            })
+            .catch((error) => {
+                console.error('Error al realizar la solicitud:', error);
+                if (error.response === undefined || error.response.data.error === undefined) {
+                    toast.error('Error al realizar la solicitud:' + error.message);
+                } else {
+                    toast.error('Error al realizar la solicitud:' + error.response.data.error);
+                }
+            });
+    };
+
+    const handleEditClick = () => {
+        setIsEditing(!isEditing);
+    };
+    
+    // obtain the user information when the user open this component
     useEffect(() => {
         getUser();
     }, []);
-
 
     const navigate = useNavigate();
 
@@ -27,15 +80,13 @@ function Profile() {
 
     const getUser = () => {
         axios
-            // cambiar el username bdp por el del ususario, habrá que investigar como mantenerlo en la sesion
-            // en un futuro usar el JWT... 
             .get(PROFILE_URL, { params: { data: getCookieValue("auth_token") } })
             .then((response) => {
                 setUsername(decodeURIComponent(response.data.username));
                 setEmail(decodeURIComponent(response.data.email));
                 setName(decodeURIComponent(response.data.name));
                 setPassword(decodeURIComponent(response.data.password));
-                setShowProfile(true)
+                setShowProfile(true);
             })
             .catch((error) => {
                 console.error('Error al realizar la solicitud:', error);
@@ -47,33 +98,91 @@ function Profile() {
             });
     }
 
-    function maskPassword(password) {
+    function maskPassword() {
         return "*********";
     }
 
     return (
         <div className='back'>
             <div className="page">
-                <Logo onClick={handleLogoClick} /> {/* Agregar el componente Logo aquí */}
+                <Logo onClick={handleLogoClick} />
                 <div className="display_div">
-                    <div className="log">Su perfil</div>
+                    {!isEditing
+                        && <div className="log">Su perfil</div>}
 
-                    <div className="profile-group">
-                        <div className="tittle_profile">Nombre:</div>
-                        {showProfile && <div>{name}</div>}
-                    </div>
-                    <div className="profile-group">
-                        <div className="tittle_profile">Username:</div>
-                        {showProfile && <div>{username}</div>}
-                    </div>
-                    <div className="profile-group">
-                        <div className="tittle_profile">Email:</div>
-                        {showProfile && <div>{email}</div>}
-                    </div>
-                    <div className="profile-group">
-                        <div className="tittle_profile">Contraseña:</div>
-                        {showProfile && <div>{maskPassword(password)}</div>}
-                    </div>
+                    {!isEditing
+                        && <div className="profile-group">
+                            <div className="tittle_profile">Nombre:</div>
+                            {showProfile && <div>{name}</div>}
+                        </div>}
+                    {!isEditing
+                        && <div className="profile-group">
+                            <div className="tittle_profile">Username:</div>
+                            {showProfile && <div>{username}</div>}
+                        </div>}
+                    {!isEditing
+                        && <div className="profile-group">
+                            <div className="tittle_profile">Email:</div>
+                            {showProfile && <div>{email}</div>}
+                        </div>}
+                    {!isEditing
+                        && <div className="profile-group">
+                            <div className="tittle_profile">Contraseña:</div>
+                            {showProfile && <div>{maskPassword()}</div>}
+                        </div>}
+                    {isEditing && (
+                        <div className="container">
+                            <h2>Por favor introduzca sus datos</h2>
+                            <form onSubmit={handleSubmit}>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    placeholder={name}
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                    defaultValue={name}
+                                    className="input"
+                                />
+                                <input
+
+                                    type="text"
+                                    name="username"
+                                    placeholder={username}
+                                    value={formData.username}
+                                    defaultValue={username}
+                                    onChange={handleChange}
+                                    className="input"
+                                />
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder={email}
+                                    value={formData.email}
+                                    defaultValue={email}
+                                    onChange={handleChange}
+                                    className="input"
+                                />
+                                <input
+                                    type="password"
+                                    name="password"
+                                    placeholder={password}
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    defaultValue={password}
+                                    className="input"
+                                />
+                                <button type="submit" className="button">
+                                    Guardar
+                                </button>
+                                <button className="button_profile" onClick={handleEditClick}>
+                                    Cancelar
+                                </button>
+                            </form>
+                            <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
+                        </div>
+                    )}
+                    {!isEditing &&
+                    <button className="button_profile" onClick={handleEditClick}> Editar</button>}
                 </div>
             </div>
             <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} />
